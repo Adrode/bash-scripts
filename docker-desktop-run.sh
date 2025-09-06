5dfb5cb895db2f93ff6f697a5b56010124e24d7c
Adrian <adrianwozniak20@gmail.com>
2025-09-06 11:56:33 +0200

    Running Docker through script

--- docker-desktop-run.sh
+++ docker-desktop-run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "Enabling docker-desktop..."
+if systemctl --user is-active --quiet docker-desktop; then
+	echo "Docker has been already started. Exiting script"
+	exit 0
+fi
+
+echo "Starting Docker..."
 systemctl --user start docker-desktop
+
+echo "Loading Docker GUI. Happy hacking!"
